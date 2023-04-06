3a873c18e74b84d9af3c07ee5cf60568a229c6ea
ZaferCenikli <127498312+ZaferCenikli@users.noreply.github.com>
2023-04-06 14:31:33 +0300

    Tasarim bitti

---
+++ app/src/main/java/com/first/sayitahminproject/SonucSayfasi.kt
@@ -0,0 +1,11 @@
+package com.first.sayitahminproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SonucSayfasi : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sonuc_sayfasi)
+    }
+}
---
+++ app/src/main/java/com/first/sayitahminproject/TahminSayfasi.kt
@@ -0,0 +1,11 @@
+package com.first.sayitahminproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TahminSayfasi : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tahmin_sayfasi)
+    }
+}
